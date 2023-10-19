65b3a5cb1dbe8d2bc111a482a07be927119c0948
hirie <hirie@HIRI-PC>
2023-10-19 20:10:05 +0300

    Add project files.

---
+++ Hirieasu_Andrei_Lab2/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace Hirieasu_Andrei_Lab2.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
